427e563a45fc6fd1990df2c0d76f883ba50ce7dc
kemijarvi <38113783+kemijarvi@users.noreply.github.com>
2018-05-03 19:34:33 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -69,7 +69,51 @@ client.on("message", async message => {
     message.reply(`${member.user.tag} is gona because of ${message.author.tag} because: ${reason}`);
 
   }
+let PREFIX = 'j'
 
+client.on('message', message => {
+  
+  if (message.content.startsWith(PREFIX + 'Userinfo')) {
+
+    const UserInfo = new Discord.MessageEmbed()
+
+        
+
+        .setAuthor(message.author.username, message.author.avatarURL())  
+        .setTitle('UserInfo')
+        .setURL('www.google.com') 
+        .setColor('RANDOM') 
+        .setImage(message.author.avatarURL()) 
+        .setThumbnail(message.author.avatarURL()) 
+
+        
+
+        .addField('Avatar', message.author.avatar, true) 
+        .addField('AvatarURL', message.author.avatarURL({
+            format: 'png'
+        }), true) 
+        .addField('AvatarURL', message.author.avatarURL({
+            size: '2048'
+        }), true)
+        .addField('Bot', message.author.bot, true) 
+        .addField('Created At', message.author.createdAt, false) 
+        .addField('Discrim', message.author.discriminator, true) 
+        .addField('DMChannel', message.author.dmChannel) 
+        .addField('ID', message.author.id) 
+        .addField('Last Message', message.author.lastMessage) 
+        .addField('Last Message ID', message.author.lastMessageID) 
+        .addField('Presence', message.author.presence) 
+        .addField('Presence Status', message.author.presence.status) 
+        .addField('Presence Game', message.author.presence.activity.name) 
+        .addField('Tag', message.author.tag) 
+        .addField('Username', message.author.username) 
+        .addField('Nick Name', message.guild.member(target).displayName) 
+
+        .setFooter('Requested By', message.author.tag) 
+        .setTimestamp() 
+
+    message.channel.send(UserInfo);
+}
   
   if(command === "ban") {
     if(!message.member.roles.some(r=>["lil"].includes(r.name)) )
@@ -115,5 +159,4 @@ client.on("message", async message => {
 });	  
 
 	  
-client.login(config.token); 
-           
+client.login(config.token);        
